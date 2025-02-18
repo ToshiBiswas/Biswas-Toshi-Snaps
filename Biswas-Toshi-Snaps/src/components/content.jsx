314a5a1376f45b1dfb22c673cc0270651107e68a
ToshiBiswas <92240916+ToshiBiswas@users.noreply.github.com>
2025-02-17 22:43:35 -0700

    fixed bugs

--- Biswas-Toshi-Snaps/src/components/content.jsx
+++ Biswas-Toshi-Snaps/src/components/content.jsx
@@ -6,17 +6,17 @@ const Content = ({ items }) => {
   return (
     <div className="main">
         {items.map((item) => (
-            <div className="main_section">
+            <div className="main_section" key={item.id}>
                 <div className="main_section_top">
                     <img src={item.photo} alt={item.description} className="main_section_top_img"></img>
-                    <div class="main_section_top_author">{item.photographer}</div>
+                    <div className="main_section_top_author">{item.photographer}</div>
                 </div>
                 <div className="main_section_bottom">
                   {item.tags.map((tag) => (
                     <button key={tag} className="main_section_bottom_button">
                       {tag}
                     </button>
-                  ))}
+                  ))} 
                 </div>
             </div>
         ))}
